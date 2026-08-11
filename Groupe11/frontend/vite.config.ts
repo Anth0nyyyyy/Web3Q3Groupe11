8c726f7e900c0de6d3fc35ce597ebb2073e1197e
Anth0nyyyyy <la238361@student.helha.be>
2026-08-11 18:12:25 +0200

    Structure propre et aplatie pour déploiement Q3

--- Groupe11/frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-// NOUVELLE VERSION CORRIGÉE
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <-- Le plugin correct, sans "-swc"
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
